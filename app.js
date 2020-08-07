4868c7805a074b4eb1a062fd2321e91e4823f088
dhruv-dc <53862511+dhruv-dc@users.noreply.github.com>
2020-08-07 13:23:38 +0530

    Initial commit -Dhruv

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var createError = require('http-errors');
-var bodyParser = require('body-parser');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var cors = require('cors')
+var createError = require("http-errors");
+var bodyParser = require("body-parser");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var cors = require("cors");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/idedb');
+mongoose.connect(
+  "mongodb+srv://dbDhruv:codeaio12345@cluster0.ayxnv.mongodb.net/dbDhruv?retryWrites=true&w=majority"
+);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var taskRouter = require('./routes/task');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var taskRouter = require("./routes/task");
 
 var app = express();
 
@@ -20,34 +22,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(cors({origin: '*'}));
-app.use(logger('dev'));
+app.use(cors({ origin: "*" }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/task', taskRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/task", taskRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
